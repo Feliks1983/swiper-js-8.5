a0c2e37422350edaebdccad9429a224dea60367f
Feliks1983 <egaifelya@gmail.com>
2026-03-26 22:29:48 +0900

    'message'

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
 const readmore = document.querySelector(".readmore-remove");
-const cardsGroup = document.querySelector(".cards-none__block");
+const cardsGroup = document.querySelector(".cards-group");
+const cardItems = document.querySelectorAll(".cards-item");
 const readmoreText = document.querySelector(".readmore__text");
 
 readmore.addEventListener("click", function () {
-	if (cardsGroup.style.display === "block") {
-		cardsGroup.style.display = "none";
-		readmoreText.innerHTML = "Показать";
-	} else {
-		cardsGroup.style.display = "block";
-		readmoreText.innerHTML = "Скрыть";
-	}
+	toggleLastThree();
 });
 
-const swiper = new Swiper(".swiper", {
+function toggleLastThree() {
+	for (let i = 8; i < cardItems.length; i++) {
+		const item = cardItems[i];
+		console.log(item);
+		if (item.style.display === "block") {
+			item.style.display = "none";
+			console.log(item);
+			readmoreText.innerHTML = "Показать все";
+		} else {
+			item.style.display = "block";
+			console.log(item);
+			readmoreText.innerHTML = "Скрыть";
+		}
+	}
+}
+
+function checkWidthOnLoad() {
+	if (window.innerWidth < 1120) {
+		for (let i = 8; i < cardItems.length; i++) {
+			cardItems[i].style.display = "none";
+		}
+		readmoreText.textContent = "Показать все";
+	} else {
+		readmore.style.display = "block"; 
+}
+	if (window.innerWidth > 1120) {
+		for (let i = 8; i < cardItems.length; i++) {
+			cardItems[i].style.display = "block";
+		}
+		readmoreText.textContent = "Скрыть";
+	} else {
+		readmore.style.display = "block";
+	}
+}
+
+checkWidthOnLoad();
+window.addEventListener("resize", checkWidthOnLoad);
+
+const swiper = new Swiper(".cards-group", {
 	slidesPerView: 2,
 	spaceBetween: 16,
 
@@ -21,5 +54,16 @@ const swiper = new Swiper(".swiper", {
 		clickable: true,
 		type: "bullets",
 	},
-});
 
+	breakpoints: {
+		320: {
+			enabled: true,
+		},
+		768: {
+			enabled: false,
+		},
+		1120: {
+			enabled: false,
+		},
+	},
+});
