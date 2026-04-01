ca6508b3a42ea422fcdc3503d09957d32021d614
Feliks1983 <egaifelya@gmail.com>
2026-04-01 21:51:17 +0900

    'message'

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const readmoreText = document.querySelector(".readmore__text");
 const cardItems9 = document.querySelectorAll(".cards-item:nth-child(n + 9)");
 const cardItems7 = document.querySelectorAll(".cards-item:nth-child(n + 7)");
 
-// Функция для переключения видимости элементов
 function toggleCards(items, show) {
 	items.forEach((item) => {
 		item.style.display = show ? "block" : "none";
@@ -13,13 +12,11 @@ function toggleCards(items, show) {
 }
 
 function handleReadMore() {
-	// Определяем, какие именно карточки мы сейчас контролируем в зависимости от ширины
 	const isMobile = window.innerWidth < 1120;
 	const targetItems = document.querySelectorAll(
 		isMobile ? ".cards-item:nth-child(n + 7)" : ".cards-item:nth-child(n + 9)",
 	);
 
-	// Проверяем текущее состояние по первому элементу в списке
 	const isHidden = targetItems[0].style.display === "none";
 
 	if (isHidden) {
@@ -36,12 +33,10 @@ function setupView() {
 	const items7 = document.querySelectorAll(".cards-item:nth-child(n + 7)");
 	const items9 = document.querySelectorAll(".cards-item:nth-child(n + 9)");
 
-	// Сбрасываем всё в исходное состояние (показываем все)
 	document
 		.querySelectorAll(".cards-item")
 		.forEach((el) => (el.style.display = "block"));
 
-	// Скрываем нужные в зависимости от экрана
 	if (isMobile) {
 		toggleCards(items7, false);
 	} else {
@@ -50,11 +45,9 @@ function setupView() {
 	readmoreText.innerHTML = "Показать все";
 }
 
-// Слушатели событий
 readmore.addEventListener("click", handleReadMore);
 window.addEventListener("resize", setupView);
 
-// Инициализация при загрузке
 setupView();
 
 const swiper = new Swiper(".cards-swiper", {
